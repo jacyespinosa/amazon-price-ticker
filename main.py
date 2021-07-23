14c742d69d8f2295f6e280993f3ec1cace8df2c4
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-22 20:41:07 -0700

    Make soup using lxml

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import lxml as lxml
 import smtplib
 from bs4 import BeautifulSoup
 
-TARGET_PRICE = 25
+TARGET_PRICE = "ENTER THE TARGET PRICE"
 MY_EMAIL = "ENTER EMAIL ADDRESS"
 MY_PASSWORD = "ENTER PASSWORD"
 RECIPIENT = "uENTER RECIPIENT'S EMAIL ADDRESS"
@@ -16,3 +16,6 @@ response = requests.get(URL, headers={"Accept-Language": "en-US,en;q=0.9",
                                       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
                                       "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"})
 webpage = response.text
+#SINCE THE HTML.PARSER DOESN'T WORK WITH AMAZON, LXML MUST BE USED INSTEAD.
+soup = BeautifulSoup(webpage, "lxml")
+find_price = soup.find_all(name="span", class_="a-size-medium a-color-price priceBlockBuyingPriceString")
